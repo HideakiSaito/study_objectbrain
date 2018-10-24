514c4f64d1d17a1e40870ae3b27560ff37d9a2a2
hidepon3110 <hidepon3110@gmail.com>
2018-10-24 21:01:45 +0900

    first commit & VSCODE-debug-config & main.rb add

---
+++ main.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+puts "hello ruby wolrd"
+
+#
+def hr
+  hr_mark = "-------------------------------------------"
+  puts hr_mark
+end
+
+hr##
+#読み込みたいファイルはこれでいけるっ
+lesson3_3 = "./Chapter03/lesson3_3/manager.rb"
+puts lesson3_3
+load lesson3_3
+hr##
+
